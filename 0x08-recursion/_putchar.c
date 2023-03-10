0d0fc7f7419a8879cfb0d4ccbc06cb153ff758a2
Bby-onnza <pandaonele0@gmail.com>
2023-03-09 16:00:13 -0800

    0x08. C - Recursion

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,11 @@
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character of print
+ *
+ * Return: on seccess 1.
+ * on error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
